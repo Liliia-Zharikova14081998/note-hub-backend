b851479943d0a7575b4f093bf16f361dbba8d18c
Liliia <liliyamalishnya@gmail.com>
2025-11-21 11:08:57 +0200

    done hw

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser } from '../controllers/authController.js';
+import { authenticate } from '../middleware/authenticate.js';
+
+const router = Router();
+
+router.use('/notes', authenticate);
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+
+router.post('/auth/logout', logoutUser);
+
+router.post('/auth/refresh', refreshUserSession);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,6 +16,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 
 app.use((req, res, next) => {
@@ -21,6 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
